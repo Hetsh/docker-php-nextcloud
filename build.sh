f9b9342aa405af6db0a86c17a77dac32f40e6596
Hetsh <git@hetsh.de>
2020-07-26 14:04:25 +0200

    Tag image with repository

--- build.sh
+++ build.sh
@@ -18,10 +18,13 @@ if ! docker version &> /dev/null; then
 	exit -1
 fi
 
+# Build the image
 APP_NAME="php-fpm-nextcloud"
-docker build --tag "$APP_NAME" .
+APP_TAG="hetsh/$APP_NAME"
+docker build --tag "$APP_TAG" .
 
 if confirm_action "Test image?"; then
+	# Start the test
 	docker run \
 	--rm \
 	--tty \
@@ -29,5 +32,5 @@ if confirm_action "Test image?"; then
 	--publish 9000:9000/tcp \
 	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 	--name "$APP_NAME" \
-	"$APP_NAME"
+	"$APP_TAG"
 fi
